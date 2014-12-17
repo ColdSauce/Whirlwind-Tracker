47c72f5024483231895d18171981d1a1f16eedc0
Stefan <soccerfanatic1996@gmail.com>
2014-12-16 19:12:05 -0500

    added requests

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def root():
+    info_hash = request.args.get('info_hash')
+    peer_id = request.args.get('peer_id')
+    port = request.args.get('port')
+    uploaded = request.args.get('uploaded')
+    downloaded = request.args.get('downloaded')
+    left = request.args.get('left')
+    compact = request.args.get('compact')
+    no_peer_id = request.args.get('no_peer_id')
+    event = request.args.get('event')
+    ip = request.args.get('ip')
+    numwant = request.args.get('numwant')
+    key = request.args.get('key')
+    trackerid = request.args.get('trackerid')
+
+    
+
 
 if __name__ == '__main__':
     app.run(debug = True)
